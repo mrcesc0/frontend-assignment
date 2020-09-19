0f9fa32a2fee282cb7f48460577e9f6997028a83
Francesco De Filippis <francescodefilippis92@gmail.com>
2020-09-19 12:13:43 +0200

    feat(app): add webpack style loaders | add page layout | add static search input

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,5 +1,35 @@
 import React from "react";
+import { Layout, PageHeader, Input, Row, Col } from "antd";
 
-export default function App() {
-  return <h1>Hello, world!!</h1>;
-}
+import "./App.css";
+
+const { Content } = Layout;
+
+const App = () => (
+  <Layout>
+    <PageHeader
+      className="header"
+      title="Pokèmons"
+      subTitle="Gotta catch'em all"
+    />
+    <Content className="content">
+      <Row>
+        <Col span={8} offset={8}>
+          <Input.Group size="large">
+            <Input.Search
+              className="input-search-pokemons"
+              placeholder="Search a pokemon..."
+              defaultValue=""
+              size="large"
+              allowClear={true}
+              minLength={3}
+              loading={false}
+            />
+          </Input.Group>
+        </Col>
+      </Row>
+    </Content>
+  </Layout>
+);
+
+export default App;
